03f86c6c7d2dd5bb078a4116cc8af3cdcf31512c
yude <ted.yude@gmail.com>
2017-09-02 10:57:56 +0800

    added README.mk

---
+++ README.mk
@@ -0,0 +1,4 @@
+# A demo of deploy flask app to docker
+
+Run `docker build .`, build and run the generated image, 
+to reproduce [issue#18](https://github.com/tiangolo/uwsgi-nginx-flask-docker/issues/18)
